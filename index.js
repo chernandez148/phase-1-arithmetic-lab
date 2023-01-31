3acf1999e5847374bf38e59124512d295be312ec
Christian Hernandez <hernandezchristian94@gmail.com>
2023-01-30 17:35:42 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 8;
+let num4 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let mod = num3 / num4;
+console.log(mod);
+
+let max = Math.max(2, 5, 6, 8, 14, 20);
+console.log(max);
